e533e03fdb411999b21d337e55f83c5dc9b095f4
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-05 19:57:35 -0500

    added first few bits of Java and some CSS

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var beginQuizButton = document.querySelector("#begin-quiz-btn");
+
+var beginQuiz = function (){
+    window.alert("button has been pressed!")
+}
+
+beginQuizButton.addEventListener("click", beginQuiz);
