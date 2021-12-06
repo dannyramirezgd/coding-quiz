be9ec95fe28bc9e18da5bb7fdb2826ce8c06dd47
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-06 17:36:22 -0500

    added first question but still needs CSS updates and functionality updates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,42 @@
 var beginQuizButton = document.querySelector("#begin-quiz-btn");
+var questionDocEl = document.querySelector("#question");
+var possibleAnswersEl = document.querySelector("#possible-answers");
 
 var beginQuiz = function (){
-    window.alert("button has been pressed!")
+    var splashScreenEl = document.getElementById("splash-screen");
+    splashScreenEl.parentNode.removeChild(splashScreenEl);
+    var instructions = document.getElementById("instructions");
+    instructions.parentNode.removeChild(instructions);
+    var beginQuizButton = document.getElementById("begin-quiz-btn");
+    beginQuizButton.parentNode.removeChild(beginQuizButton);
+    firstQuestion();
+}
+var firstQuestion = function(){
+    console.log("hello?")
+    var questionBlockEl = document.createElement("div");
+    questionBlockEl.className = "question-block";
+        questionDocEl.appendChild(questionBlockEl);
+  
+    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    var questionEl = document.createElement("li");
+    //questionEl.innerHTML = "<a href='#'>12</a>";
+    possibleAnswersEl.appendChild(questionEl);
+    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    var questionEl = document.createElement("li");
+    //questionEl.innerHTML = "<a href='#'>5</a>";
+    possibleAnswersEl.appendChild(questionEl);
+    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    var questionEl = document.createElement("li");
+    //questionEl.innerHTML = "<a href='#'>7</a>";
+    possibleAnswersEl.appendChild(questionEl);
+    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    var questionEl = document.createElement("li");
+    //questionEl.innerHTML = "<a href='#'>18</a>";
+    possibleAnswersEl.appendChild(questionEl);
+
+
+    
+
 }
 
 beginQuizButton.addEventListener("click", beginQuiz);
