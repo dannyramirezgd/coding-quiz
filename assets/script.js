593007af3c94e90e62009ba3de08b64faef17f38
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-07 17:50:29 -0500

    fixed up JS and CSS

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,46 @@
+var body = document.body;
+var startScreenEl = document.querySelector("#start-screen");
 var beginQuizButton = document.querySelector("#begin-quiz-btn");
-var questionDocEl = document.querySelector("#question");
-var possibleAnswersEl = document.querySelector("#possible-answers");
+var questions = document.querySelector("#questions");
+var questionTitleEl = document.querySelector("#question-title");
+var possibleAnswerEl = document.querySelector("#possible-answers");
 
 var beginQuiz = function (){
-    var splashScreenEl = document.getElementById("splash-screen");
-    splashScreenEl.parentNode.removeChild(splashScreenEl);
-    var instructions = document.getElementById("instructions");
-    instructions.parentNode.removeChild(instructions);
-    var beginQuizButton = document.getElementById("begin-quiz-btn");
-    beginQuizButton.parentNode.removeChild(beginQuizButton);
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.parentNode.removeChild(startScreenEl);
     firstQuestion();
 }
 var firstQuestion = function(){
-    console.log("hello?")
-    var questionBlockEl = document.createElement("div");
-    questionBlockEl.className = "question-block";
-        questionDocEl.appendChild(questionBlockEl);
+    var questionTitleEl = document.createElement("h2");
+    questionTitleEl.className = "start";
+    questionTitleEl.textContent = "How many eggs we got?"
+    questions.appendChild(questionTitleEl);
+
+    var possibleAnswerEl = document.createElement("button");
+    possibleAnswerEl.className ="begin-quiz-btn";
+    possibleAnswerEl.textContent = "Bout a dozen"
+    questions.appendChild(possibleAnswerEl);
+    
+    // var questionBlockEl = document.createElement("div");
+    // questionBlockEl.className = "question-block";
+    // questionDocEl.appendChild(questionBlockEl);
   
-    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
-    var questionEl = document.createElement("li");
-    //questionEl.innerHTML = "<a href='#'>12</a>";
-    possibleAnswersEl.appendChild(questionEl);
-    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
-    var questionEl = document.createElement("li");
-    //questionEl.innerHTML = "<a href='#'>5</a>";
-    possibleAnswersEl.appendChild(questionEl);
-    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
-    var questionEl = document.createElement("li");
-    //questionEl.innerHTML = "<a href='#'>7</a>";
-    possibleAnswersEl.appendChild(questionEl);
-    questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
-    var questionEl = document.createElement("li");
-    //questionEl.innerHTML = "<a href='#'>18</a>";
-    possibleAnswersEl.appendChild(questionEl);
+    // questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    // var questionEl = document.createElement("li");
+    // //questionEl.innerHTML = "<a href='#'>12</a>";
+    // possibleAnswersEl.appendChild(questionEl);
+    // questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    // var questionEl = document.createElement("li");
+    // //questionEl.innerHTML = "<a href='#'>5</a>";
+    // possibleAnswersEl.appendChild(questionEl);
+    // questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    // var questionEl = document.createElement("li");
+    // //questionEl.innerHTML = "<a href='#'>7</a>";
+    // possibleAnswersEl.appendChild(questionEl);
+    // questionBlockEl.innerHTML = "<p class = 'question'>How many eggs in a dozen?</p>";
+    // var questionEl = document.createElement("li");
+    // //questionEl.innerHTML = "<a href='#'>18</a>";
+    // possibleAnswersEl.appendChild(questionEl);
 
 
     
